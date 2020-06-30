aaea5fc01b7aeceac72e2a00444d59a036c0bc59
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-06-30 18:46:23 +0300

    update

---
+++ tests/CliTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class CliTest extends TestCase
+{
+    public function testCanBePromptToTheSTDIN()
+    {
+        // SAD NOTICE: I really don't know how to test this stuff...
+        $this->assertTrue(true);
+    }
+}
